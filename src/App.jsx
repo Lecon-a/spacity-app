855b7ec9217bf5653f43beb48ecbb1c67f488f69
Sunday P. Afolabi <sundayafolabi@ajeoba.com>
2023-04-17 16:34:16 -0400

    Fix routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { Route } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { Layout, LandingPage, Courses, Services, Blogs, AboutUs, ContactUs, SignUp, ForgotPassword, SingleCourse } from './components';
 
 function App() {
 
   return (
-    <div className="App">
-          <Route path="/" element={<Layout />}>
-            <Route index element={<LandingPage />} />
-            <Route path='/courses' element={<Courses />} />
-            <Route path='/courses/:id' element={<SingleCourse />} />
-            <Route path='/services' element={<Services />} />
-            <Route path='/blogs' element={<Blogs />} />
-            <Route path='/about_us' element={<AboutUs />} />
-            <Route path='/contact_us' element={<ContactUs />} />
-            {/* other pages */}
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/forgot-password' element={<ForgotPassword />} />
-          </Route>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Layout />}>
+        <Route exact index element={<LandingPage />} />
+        <Route exact path='/courses' element={<Courses />} />
+        <Route exact path='/courses/:id' element={<SingleCourse />} />
+        <Route exact path='/services' element={<Services />} />
+        <Route exact path='/blogs' element={<Blogs />} />
+        <Route exact path='/about_us' element={<AboutUs />} />
+        <Route exact path='/contact_us' element={<ContactUs />} />
+        
+        <Route exact path='/signup' element={<SignUp />} />
+        <Route exact path='/forgot-password' element={<ForgotPassword />} />
+      </Route>
+    </Routes>
   )
 }
 
 export default App
+
+/*
+
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,17 @@ import './App.css'
 import './index.css'
 import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter as Router, Routes } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-        <Router basename={window.location.pathname || "/"}>
-            <Routes>
-                <App />
-            </Routes>
-        </Router>
+        <div className="App">
+            <BrowserRouter basename={"/"}>
+                <App />   
+            </BrowserRouter>
+        </div>
     </Provider>
 )
+
+// window.location.pathname || 
